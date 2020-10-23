e022d053c462e3074e14e3914aa834febea70a12
Daniel Saputra <danielwetan.io@gmail.com>
2020-10-23 14:26:28 +0700

    Arrays

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 // mod variable;
 // mod types;
 // mod strings;
-mod tuples;
+// mod tuples;
+mod arrays;
 
 fn main() {
     // print::run();
     // variable::run();
     // types::run();
     // strings::run();
-    tuples::run();
+    // tuples::run();
+    arrays::run();
 }
